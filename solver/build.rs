68c374431bc0a11be131ae8ac09df12b900071e0
Kyrylo Gara <kvgara@gmail.com>
2025-04-10 13:29:58 +0300

    [fix] - fix solver proto compilation for protoc < 3.15.

--- solver/build.rs
+++ solver/build.rs
@@ -1,5 +1,6 @@
 pub fn main() {
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
                 "../proto/user_objective.proto",

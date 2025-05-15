c4f1c762623f76fed9a47f0964c9fe12e64656ad
Token Titan <83060531+TokenTitan@users.noreply.github.com>
2025-05-15 16:20:54 +0530

    feat: update create token mint

--- solana/programs/solana-vamp-program/src/state/vamp_state.rs
+++ solana/programs/solana-vamp-program/src/state/vamp_state.rs
@@ -2,9 +2,14 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-pub struct VampState {    // Admin address
-    pub bump: u8,              // Bump for vault authority PDA
-    pub mint: Pubkey,          // Token mint address (from fungible_token)
+pub struct VampState {
+    pub bump: u8,          
+    pub mint: Pubkey,
+    #[max_len(65)]
+    pub solver_public_key: Vec<u8>,
+    #[max_len(65)]
+    pub validator_public_key: Vec<u8>,
+    pub vamp_identifier: u64,
 }
 
 #[account]

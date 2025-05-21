ef991942cfc3dd2b01581d384a0fcf0d0e0b3aef
Evgeny Koshelev <eko2000@gmail.com>
2025-05-21 09:36:33 +0200

    Remove redundant dependencies from the balance_util

--- crates/balance_util/src/lib.rs
+++ crates/balance_util/src/lib.rs
@@ -15,21 +15,26 @@ pub fn get_balance_hash(
     Ok(hash_message.to_vec())
 }
 
-#[tokio::test]
-async fn test_get_balance_hash() {
-    // Test setup
-    let address = hex::decode("589A698b7b7dA0Bec545177D3963A2741105C7C9").unwrap();
-    let amount = 1_000_000_000u64;
-    let intent_id = hex::decode("1111111111111111111111111111111111111111111111111111111111111111").unwrap();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // Test case: Valid inputs
-    let balance_hash = get_balance_hash(&address, amount, &intent_id);
-    assert!(balance_hash.is_ok());
-    assert_eq!(
-        balance_hash.unwrap(),
-        vec![
-            181, 209, 203, 211, 173, 67, 135, 228, 171, 113, 74, 177, 223, 120, 19, 120,
-            245, 152, 134, 189, 69, 93, 73, 168, 41, 70, 164, 38, 255, 208, 97, 141
-        ]
-    );
+    #[test]
+    fn test_get_balance_hash() {
+        // Test setup
+        let address = hex::decode("589A698b7b7dA0Bec545177D3963A2741105C7C9").unwrap();
+        let amount = 1_000_000_000u64;
+        let intent_id = hex::decode("1111111111111111111111111111111111111111111111111111111111111111").unwrap();
+
+        // Test case: Valid inputs
+        let balance_hash = get_balance_hash(&address, amount, &intent_id);
+        assert!(balance_hash.is_ok());
+        assert_eq!(
+            balance_hash.unwrap(),
+            vec![
+                181, 209, 203, 211, 173, 67, 135, 228, 171, 113, 74, 177, 223, 120, 19, 120,
+                245, 152, 134, 189, 69, 93, 73, 168, 41, 70, 164, 38, 255, 208, 97, 141
+            ]
+        );
+    }
 }

29507f41e060d000c1518b440d738728809f6d8a
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2025-06-03 18:51:22 +0530

    feat: adds setter and events in Vamp contract

--- evm/src/Vamp.sol
+++ evm/src/Vamp.sol
@@ -15,29 +15,45 @@ contract Vamp is Ownable {
     event TreasurySet(address indexed newTreasury);
     event VampInitiated(address indexed vamper, address indexed vampToken);
     event FeeSet(uint256 newFee);
+    event FeeTokenSet(address feeToken);
 
-    constructor(address _treasury, uint256 _fee, address _feeToken) Ownable(msg.sender) {
-        if (_treasury == address(0) || _feeToken == address(0)) revert ZeroAddress();
+    constructor(
+        address _treasury,
+        uint256 _fee,
+        address _feeToken
+    ) Ownable(msg.sender) {
+        if (_treasury == address(0) || _feeToken == address(0))
+            revert ZeroAddress();
         treasury = _treasury;
         fee = _fee;
         feeToken = IERC20(_feeToken);
     }
 
-    function setTreasury(address newTreasury) public onlyOwner {
+    function setTreasury(address newTreasury) external onlyOwner {
         if (newTreasury == address(0)) revert ZeroAddress();
         treasury = newTreasury;
         emit TreasurySet(newTreasury);
     }
 
-    function setFee(uint256 newFee) public onlyOwner {
+    function setFee(uint256 newFee) external onlyOwner {
         fee = newFee;
         emit FeeSet(newFee);
     }
 
-    function initiateVamp(address vamper, address vampToken) public onlyOwner {
-        if (vamper == address(0) || vampToken == address(0)) revert ZeroAddress();
+    function initiateVamp(
+        address vamper,
+        address vampToken
+    ) external onlyOwner {
+        if (vamper == address(0) || vampToken == address(0))
+            revert ZeroAddress();
         bool success = feeToken.transferFrom(vamper, treasury, fee);
         if (!success) revert InsufficientBalance();
         emit VampInitiated(vamper, vampToken);
+    }
+
+    function setFeeToken(address _feeToken) external onlyOwner {
+        if (_feeToken == address(0)) revert ZeroAddress();
+        feeToken = IERC20(_feeToken);
+        emit FeeTokenSet(_feeToken);
     }
 }

bec88046a637cddb50c7cd66d1641b783a781562
Evgeny Koshelev <eko2000@gmail.com>
2025-12-26 17:05:33 +0100

    Introduce a deployer script

--- ethereum/Vamper/script/VampTokenEmitterUpgradeable.s.sol
+++ ethereum/Vamper/script/VampTokenEmitterUpgradeable.s.sol
@@ -2,18 +2,40 @@
 pragma solidity 0.8.33;
 
 import {Script, console} from "forge-std/Script.sol";
+import {BaseDeployer} from "./BaseDeployer.s.sol";
 import {VampTokenEmitterUpgradeable} from "../src/VampTokenEmitterUpgradeable.sol";
 
-contract CounterScript is Script {
-    VampTokenEmitterUpgradeable public vamper;
+contract DeployVampTokenEmitterUpgradeable is BaseDeployer {
+    function run() external {
+        uint256 deployerPrivateKey = _getPrivateKey();
+        bytes32 _salt = _generateSalt();
+        address owner = _getOwner();
+        _deploy(_salt, deployerPrivateKey, owner);
+    }
 
-    function setUp() public {}
+    function run(uint256 salt) external {
+        uint256 deployerPrivateKey = _getPrivateKey();
+        address owner = _getOwner();
+        bytes32 _salt = bytes32(salt);
+        _deploy(_salt, deployerPrivateKey, owner);
+    }
 
-    function run() public {
-        vm.startBroadcast();
+    function _deploy(bytes32 salt, uint256 deployerPrivateKey, address owner) internal {
+        for (uint256 i = 0; i < networks.length; i++) {
+            NetworkConfig memory config = networks[i];
+            console.log("Deploying VampTokenEmitterUpgradeable to:", config.name);
 
-        vamper = new VampTokenEmitterUpgradeable();
+            vm.createSelectFork(config.rpcUrl);
+            vm.startBroadcast(deployerPrivateKey);
 
-        vm.stopBroadcast();
+            address contractAddress = address(new VampTokenEmitterUpgradeable{salt: salt}());
+            address computedAddress = _computeCreate2Address(
+                salt, hashInitCode(abi.encodePacked(type(VampTokenEmitterUpgradeable).creationCode, abi.encode(owner)))
+            );
+            require(contractAddress == computedAddress, "Contract address mismatch");
+            console.log("VampTokenEmitterUpgradeable deployed to:", contractAddress);
+
+            vm.stopBroadcast();
+        }
     }
 }
